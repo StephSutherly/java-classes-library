d463feae412bad2917bb36e3ca871172bd3da721
Steph Scully <stephanielarascullion@yahoo.com>
2019-07-23 17:16:20 +0100

    Files set up and working on MVP

---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void setUp(){
+        library = new Library(100);
+        book = new Book();
+    }
+
+    @Test
+    public void
+}
